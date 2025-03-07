c847b7e026035ef9e516ce3af74933b6f7730be4
Medina Softic <medinasoftic@Medinas-MacBook-Pro.attlocal.net>
2025-03-07 00:05:38 -0600

    complete

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,38 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = 5 - 2; //This can run either way because the - operator is used for subtraction. so string or number will work. It will just look better without quotes
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = false; //This needs to have a diffrent value, we should use true or false. Not a string. (when the code runs, since its false it will not print)
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
+let age = 25; // This was inncorrect because it was a string + a number. + is used for adding numbers and adding string concatenation.
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+
+
+
+// Part 2: Write Your Own Examples
+
+
+//Null example
+let highScore = null
+console.log(highScore)
+
+
+
+// Implicit type conversion
+let playerOneScore = 100;
+let playerTwoScore = "200";
+let totalScore = playerOneScore + playerTwoScore;
+console.log(totalScore) 
+
+// Explicit type conversion
+let priceOfEggs = "2.50";
+let priceOfMilk = 3.50;
+let totalCost = Number(priceOfEggs) + priceOfMilk; //Using the Number() function to convert the string to a number.
+console.log(totalCost)
